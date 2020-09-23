d7699d72be8fff49673d87d288d5a267ce42545c
DianaRivero <65493543+DianaRivero@users.noreply.github.com>
2020-09-23 15:16:54 +0200

    Add target blank to Github link

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer(){
     return (
         <div className = "Footer">
-        <a href="https://github.com/DianaRivero/react-weather-app"
+        <a href="https://github.com/DianaRivero/react-weather-app" target = "_blank" rel="noopener noreferrer"
           >Open-source-code </a> 
         by Diana Rivero
         </div>
